45bc033aaf89ad8a7c0a82c23036d6b155c7dc4c
Sefinek <92880265+sefinek24@users.noreply.github.com>
2023-10-08 16:05:45 +0200

    Add types

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,57 @@
-export function cat(): Object;
-export function dog(): Object;
-export function fox(): Object;
-export function fish(): Object;
-export function alpaca(): Object;
-export function bird(): Object;
+declare module 'random-animals' {
+    export interface RandomAnimalResponse {
+        success: boolean;
+        status: number;
+        info: {
+            category: string;
+            endpoint: string;
+        };
+        message: string;
+    }
 
-export function apiVersion(): Object;
-export function cdn(): Object;
+    export interface ApiVersionResponse {
+        success: boolean;
+        status: number;
+        version: string;
+        available: number[];
+    }
 
-export const version: string;
+    export interface CdnVersionResponse {
+        success: boolean;
+        status: number;
+        message: string;
+        version: string;
+        worker: number;
+        contact: string;
+        domain: {
+            main: string;
+            api: string;
+            cdn: string;
+        };
+        path: {
+            static: string;
+            gitResources: string;
+            images: string;
+            temp: string;
+        };
+        other: {
+            ssl: boolean;
+            proxy: boolean;
+        };
+    }
+
+    export interface RandomAnimals {
+        cat(): Promise<RandomAnimalResponse>;
+        dog(): Promise<RandomAnimalResponse>;
+        fox(): Promise<RandomAnimalResponse>;
+        fish(): Promise<RandomAnimalResponse>;
+        alpaca(): Promise<RandomAnimalResponse>;
+        bird(): Promise<RandomAnimalResponse>;
+        apiVersion(): Promise<ApiVersionResponse>;
+        cdnVersion(): Promise<CdnVersionResponse>;
+    }
+
+    const randomAnimals: RandomAnimals;
+
+    export default randomAnimals;
+}
