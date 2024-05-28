62a5ddfdd80220b2c4272c151620d66500f1d044
Sefinek <contact@sefinek.net>
2024-05-28 17:37:39 +0200

    Move docs to index.d.ts

--- index.js
+++ index.js
@@ -69,65 +69,22 @@ const getData = async endpoint => {
 
 // Export functions and values for external use
 module.exports = {
-	/**
-	 * Fetch random data for a cat from the external API.
-	 * @returns {Promise} - A promise that resolves with random cat data or rejects with an error.
-	 */
 	cat: () => getData('cat'),
-
-	/**
-	 * Fetch random data for a dog from the external API.
-	 * @returns {Promise} - A promise that resolves with random dog data or rejects with an error.
-	 */
 	dog: () => getData('dog'),
-
-	/**
-	 * Fetch random data for a fox from the external API.
-	 * @returns {Promise} - A promise that resolves with random fox data or rejects with an error.
-	 */
 	fox: () => getData('fox'),
-
-	/**
-	 * Fetch random data for a fish from the external API.
-	 * @returns {Promise} - A promise that resolves with random fish data or rejects with an error.
-	 */
 	fish: () => getData('fish'),
-
-	/**
-	 * Fetch random data for an alpaca from the external API.
-	 * @returns {Promise} - A promise that resolves with random alpaca data or rejects with an error.
-	 */
 	alpaca: () => getData('alpaca'),
-
-	/**
-	 * Fetch random data for a bird from the external API.
-	 * @returns {Promise} - A promise that resolves with random bird data or rejects with an error.
-	 */
 	bird: () => getData('bird'),
 
-	/**
-	 * Get the API version from the external API.
-	 * @returns {String} - A promise that resolves with the API version or rejects with an error.
-	 */
 	apiVersion: async () => {
 		const url = `${API_URL}/api`;
 		const jsonData = await makeRequest(url);
 		return jsonData.version;
 	},
-
-	/**
-	 * Get the CDN version from the external CDN.
-	 * @returns {String} - A promise that resolves with the CDN version or rejects with an error.
-	 */
 	cdnVersion: async () => {
 		const url = 'https://cdn.sefinek.net';
 		const jsonData = await makeRequest(url);
 		return jsonData.message;
 	},
-
-	/**
-	 * Get module version.
-	 * @returns {String} - Returns this package version.
-	 */
 	version
 };
